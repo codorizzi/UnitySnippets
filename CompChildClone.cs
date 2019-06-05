f28a912f7a2ff141746e9d46bd910c9c3711a6e1
codorizzi <curtis@odorizzi.ca>
2019-06-05 12:31:37 -0700

    Add files via upload

---
+++ CompChildClone.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Utils;
+
+public class CompChildClone : MonoBehaviour {
+    
+    public List<Component> ignore;
+
+    void Start() {
+        foreach(Transform child in transform) {
+            foreach (Component component in gameObject.GetComponents(typeof(Component))) {
+
+                if (ignore.Contains(component))
+                    continue;
+                
+                ComponentUtil.CopyComponent(component, child.gameObject);
+            }
+        }
+    }
+    
+}
