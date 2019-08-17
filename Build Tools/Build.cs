0eb148859d92a11d63321365438c82e0e66712c8
codorizzi <curtis@odorizzi.ca>
2019-08-17 15:57:49 -0700

    Add files via upload

--- Build Tools/Build.cs
+++ Build Tools/Build.cs
@@ -1,4 +1,5 @@
 ﻿#if (UNITY_EDITOR)
+using System;
 using File = UnityEngine.Windows.File;
 using UnityEditor;
 using UnityEngine;
@@ -9,33 +10,63 @@ using System.IO;
 // Requirements: Butler, 7Zip
 
 public static class ScriptBatch {
-    
+
     // application paths
     private static string _zipPath = "C:\\Program Files\\7-Zip\\7z.exe";
     private static string _butlerPath = "C:\\Program Files\\butler-windows-amd64\\butler.exe";
     
     // itch paths
     private static string _itchLogin = "searle";
-    private static string _projectName = "platformer2";
+    private static string _projectName = Application.productName.ToLower();
 
     [MenuItem("BuildTools/Butler WebGL")]
-    public static void BuildGame() {
+    public static void BuildWebGL() {
 
+        BuildGame(BuildTarget.WebGL);
+    }
+
+    [MenuItem("BuildTools/Butler Windows")]
+    public static void BuildWindows() {
+        BuildGame(BuildTarget.StandaloneWindows);
+    }
+
+    public static void BuildGame(BuildTarget buildTarget) {
+        
         DirectoryInfo di = new DirectoryInfo(Application.dataPath);
         string cwd = di.Parent.FullName;
 
-        UnityEngine.Debug.Log("Building WebGL: " + cwd);
+        string buildExtension;
+        string buildPath;
+        switch (buildTarget) {
+            case BuildTarget.WebGL:
+                buildExtension = "webgl";
+                buildPath = $"{buildExtension}";
+                break;
+            case BuildTarget.StandaloneWindows:
+                buildExtension = "win";
+                buildPath = $"{buildExtension}/{_projectName}_{buildExtension}.exe";
+                break;
+            default:
+                throw new Exception("Invalid Target Type");
+        }
+        
+        UnityEngine.Debug.Log($"Building {buildExtension}: " + cwd);
 
-        File.Delete("./Builds/webgl.zip");
-        BuildPipeline.BuildPlayer(getScenes(), "./Builds/webgl", BuildTarget.WebGL, BuildOptions.None);
+        string basename = $"{_projectName}_{buildExtension}";
+        string zipPath = "./Builds/" + basename + ".zip";
+
+        Directory.Delete($"./Builds", true);
+        
+        BuildPipeline.BuildPlayer(getScenes(), $"./Builds/{buildPath}", buildTarget, BuildOptions.None);
 
         UnityEngine.Debug.Log("Compressing build");
-        executeProcess(_zipPath, "a -tzip -r ./Builds/webgl ./Builds/webgl");
+        executeProcess(_zipPath, $"a -tzip -r ./Builds/{basename} ./Builds/{buildExtension}");
 
-        UnityEngine.Debug.Log("Pushing to Itch: " + $"{_butlerPath} push {cwd}\\Builds\\webgl.zip {_itchLogin}/{_projectName}:webgl");
-        executeProcess(_butlerPath, $"push {cwd}\\Builds\\webgl.zip {_itchLogin}/{_projectName}:webgl");
+        UnityEngine.Debug.Log("Pushing to Itch: " + $"{_butlerPath} push {cwd}\\Builds\\{basename}.zip {_itchLogin}/{_projectName}:{buildExtension}");
+        executeProcess(_butlerPath, $"push {cwd}\\Builds\\{basename}.zip {_itchLogin}/{_projectName}:{buildExtension}");
 
-        UnityEngine.Debug.Log("WebGL deploy complete");
+        UnityEngine.Debug.Log($"{buildExtension} deploy complete");
+        
     }
 
     public static string[] getScenes() {
